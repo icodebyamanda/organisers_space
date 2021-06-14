123dd87de47087d3e065e09111b37aa074e17961
icodebyamanda <rasoamanda@gmail.com>
2021-06-14 17:22:49 +0200

    #1 checking if react was working by creating a component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import React from 'react';
+import Welcome from './components/Welcome'
 
 function App() {
   return (
     <div className="App">
       
+    <Welcome />
+
     </div>
   );
 }
---
+++ client/src/components/Welcome.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react'
+
+export default function Instructors() {
+
+  return(
+    <div>
+
+      <h1>Hello World</h1>
+
+    </div>
+  )
+
+}
