54ce41f6b2a4a9d0c24035630294d4c7ee342f41
icodebyamanda <rasoamanda@gmail.com>
2021-06-14 20:11:29 +0200

    #7 Events model created and migrated

---
+++ models/event.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Event extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Event.init({
+    profile_id: DataTypes.INTEGER,
+    title: DataTypes.STRING,
+    description: DataTypes.STRING,
+    audience_capacity: DataTypes.INTEGER,
+    if_underage: DataTypes.BOOLEAN,
+    materials_needed: DataTypes.STRING,
+    if_free: DataTypes.BOOLEAN,
+    pricing: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Event',
+  });
+  return Event;
+};
