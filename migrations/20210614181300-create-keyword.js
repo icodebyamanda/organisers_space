15972e9c29cc4663ebdbc7c707a5bdfaed62881f
icodebyamanda <rasoamanda@gmail.com>
2021-06-14 20:18:48 +0200

    #7 Keyword model created and migrated

---
+++ migrations/20210614181300-create-keyword.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Keywords', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      event_id: {
+        type: Sequelize.INTEGER
+      },
+      profile_id: {
+        type: Sequelize.INTEGER
+      },
+      word: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Keywords');
+  }
+};
