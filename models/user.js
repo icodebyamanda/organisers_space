c7b86f5bfc7f60c34064c179cea44b6e1ba957aa
icodebyamanda <rasoamanda@gmail.com>
2021-06-15 08:57:50 +0200

    #9 users route with jwt and association models between users and profiles done

--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
 'use strict';
 const { Model } = require('sequelize');
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -9,13 +12,21 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      User.hasMany(models.Profile, { foreignKey: 'UserId' });
     }
   };
   User.init({
     firstname: DataTypes.STRING,
     lastname: DataTypes.STRING,
     email: DataTypes.STRING,
-    password: DataTypes.STRING
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      set(value) {
+        const hash = bcrypt.hashSync(value, saltRounds);
+        this.setDataValue("password", hash);
+      },
+    },
   }, {
     sequelize,
     modelName: 'User',
--- routes/users.js
+++ routes/users.js
@@ -4,12 +4,12 @@ const models = require('../models');
 // const { sequelize } = require("../models");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-// const bcrypt = require("bcrypt");
-// const saltRounds = 10;
-// const supersecret = process.env.SUPER_SECRET;
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const supersecret = process.env.SUPER_SECRET;
 //const userShouldBeLoggedIn = require("../guards/userShouldBeLoggedIn");
 
-/* GET users listing. */
+//! GET users listing
 router.get("/", function (req, res) {
 	models.User.findAll()
 		.then((data) => res.send(data))
@@ -18,29 +18,107 @@ router.get("/", function (req, res) {
 		});
 });
 
-// POST Register a user
+//! POST Register a user
 
-router.post("/register", async (req, res) => {
+router.post('/register', async (req, res) => {
   const { firstname, lastname, email, password } = req.body;
 
   try {
 
     await models.User.create({ firstname, lastname, email, password });
-
     res.send({ message: "Register successful" });
+
   } catch (err) {
     res.status(400).send({ message: err.message });
   }
 });
 
+//! POST - Log a user
 
-// Log a user
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
 
-// Get user's settings profile
+  try {
+    const user = await models.User.findOne({ where: { email } });
+
+    if (user) {
+      const user_id = user.id;
+      const correctPassword = await bcrypt.compare(password, user.password);
+      if (!correctPassword) throw new Error("Incorrect password");
+      var token = jwt.sign({ user_id }, supersecret);
+
+      res.send({ message: "Login successful, here is your token", token });
+    } else {
+      throw new Error("User does not exist");
+    }
+  } catch (err) {
+    res.status(400).send({ message: err.message });
+  }
+});
+
+//! GET user's settings' profile // TBC for token <- guard
+
+router.get('/settings/:id', async (req, res) => {
+  // const id = req.user_id;
+  const {id} = req.params;
+
+  try {
+    const user = await models.User.findOne({
+        where: {
+          id,
+        },
+      });
+    res.send(user);
+    } 
+    catch (error) {
+      res.status(500).send(error);
+    }
+  });
 
 
-// Update user's data (Private route only - no password reset)
+//! PUT - Update user's data (Private route only - no password reset) <- Guard
 
-// Delete a user
+  router.put('/settings/:id', async (req, res) => {
+    const {firstname, lastname, email, password} = req.body;
+    // const id = req.user_id;
+    const {id} = req.params;
+
+    try {
+      //const hash = await bcrypt.hash(password, saltRounds);
+      await models.User.update(
+        { firstname, lastname, email, password },
+      // { email, username, password : hash },
+      { where: {
+          id,
+        },
+      });
+
+      res.send({ message: "user updated succesfully!" });
+    }
+    catch(error) { 
+      res.status(500).send(error);
+    }
+  });
+
+
+//! DELETE a user -> guard
+
+router.delete('/settings/:id', async (req, res) => {
+  // const id = req.user_id;
+  const {id} = req.params;
+
+  try {
+
+  const user = await models.User.destroy({
+    where: {
+      id,
+    }
+    // include: models.Selection,
+  });
+    res.send({message:'User deleted'});
+  
+  } catch(err) { 
+    res.status(500).send(err)};
+});
 
 module.exports = router;
