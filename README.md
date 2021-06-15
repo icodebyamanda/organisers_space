8dd534b017a695abeecfab465800301e30db295b
icodebyamanda <rasoamanda@gmail.com>
2021-06-15 07:47:25 +0200

    #9 users route WIP

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # organisers_space
+
+Database prep
+
+npm install sequelize mysql2
+
+Create a local MySQL database.
+Add a .env to your root folder containing the MySQL authentication information for the root user as well as the name of your database. For example:
+
+DB_HOST=localhost
+DB_USER=root
+DB_PASS=YOURPASSWORD
+DB_NAME=YOURDATABASE
+SUPER_SECRET=shhhhhhh
+
+Run npm run migrate in your terminal in order to create the DB tables.
+
+npm install jsonwebtoken bcrypt
+npm install axios
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,46 @@
 var express = require('express');
 var router = express.Router();
-var models = require('../models');
-
+const models = require('../models');
+// const { sequelize } = require("../models");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+// const bcrypt = require("bcrypt");
+// const saltRounds = 10;
+// const supersecret = process.env.SUPER_SECRET;
+//const userShouldBeLoggedIn = require("../guards/userShouldBeLoggedIn");
 
 /* GET users listing. */
-router.get('/', (req, res, next) => {
-  res.send('respond with a user');
+router.get("/", function (req, res) {
+	models.User.findAll()
+		.then((data) => res.send(data))
+		.catch((error) => {
+			res.status(500).send(error);
+		});
 });
+
+// POST Register a user
+
+router.post("/register", async (req, res) => {
+  const { firstname, lastname, email, password } = req.body;
+
+  try {
+
+    await models.User.create({ firstname, lastname, email, password });
+
+    res.send({ message: "Register successful" });
+  } catch (err) {
+    res.status(400).send({ message: err.message });
+  }
+});
+
+
+// Log a user
+
+// Get user's settings profile
+
+
+// Update user's data (Private route only - no password reset)
+
+// Delete a user
 
 module.exports = router;
